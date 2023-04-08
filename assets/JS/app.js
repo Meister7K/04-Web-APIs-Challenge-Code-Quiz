01563dbae1f9847ea9ca0e8a8d3699582352b633
7K <kmanfinkel17@gmail.com>
2023-04-08 14:10:22 -0500

    "added photos and created home screen"

---
+++ assets/JS/app.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+    // Need timer (act 9/10)
+    // need score holding/sharing variable
+
+// SO THAT I can gauge my progress compared to my peers
+// Acceptance Criteria
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+    // event listener (act 11/12)
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+    // append
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+    // if else function
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
